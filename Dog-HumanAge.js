4059865bf6fb3fedf4e3aadab404ce870a62df14
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2020-12-21 15:20:39 +0300

    Update Dog-HumanAge.js

--- Dog-HumanAge.js
+++ Dog-HumanAge.js
@@ -2,6 +2,6 @@
 //humanAge = (dogAge -2) * 4 + 21
 
 var dogAge = prompt("Hey, how old is your dog?")
-var humanAge = (dogAge -2) * 4 + 21;
+var humanAge = (dogAge -2) * 4 + 24;
 
 alert("If your dog was a human it would be " + humanAge + " years old.")
