841822a522a0655764d276bbca8b08886d68535b
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2020-12-21 15:07:47 +0300

    Dog Age to Human Age Formula

---
+++ Dog-HumanAge.js
@@ -0,0 +1,7 @@
+//Dog Age to Human Age Formula
+//humanAge = (dogAge -2) * 4 + 21
+
+var dogAge = prompt("Hey, how old is your dog?")
+var humanAge = (dogAge -2) * 4 + 21;
+
+alert("If your dog was a human it would be " + humanAge + " years old.")
