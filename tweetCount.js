9291577517d6ca5917f147eb2abca23a1c3168ac
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2020-12-21 13:25:16 +0300

    Update tweetCount.js

--- tweetCount.js
+++ tweetCount.js
@@ -1,1 +1,7 @@
 var tweet = prompt("Hey there! Compose your tweet");
+var tweetCount = tweet.length;
+var tweetRemaining = 280 - tweetCount;
+
+//The twitter character count limit it 280 characters
+
+alert(" You have written" + tweetCount + " characters. You have " + tweetRemaining + "Characters remaining."
