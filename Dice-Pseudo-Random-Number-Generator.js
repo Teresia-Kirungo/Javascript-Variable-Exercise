070808e986d52b9705009e0b554c1424a438ed67
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-01 08:34:12 +0300

    Update Dice-Pseudo-Random-Number-Generator.js

--- Dice-Pseudo-Random-Number-Generator.js
+++ Dice-Pseudo-Random-Number-Generator.js
@@ -1,2 +1,6 @@
-n = Math.random();
-// The Math.random() function returns a floating-point, pseudo-random number in the range 0 to less than 1 (inclusive of 0, but not 1) with approximately uniform distribution over that range — which you can then scale to your desired range.
+// A Dice has 1 to 6
+n = Math.random(); // generate a random number.
+// The Math.random() function returns a random floating-point(decimal point), in the range 0 to less than 1 (0 to5) with approximately uniform distribution over that range — which you can then scale to your desired range.
+n = n * 6; //making the number greater than 1 which will generate 0 to 5
+n = Math.floor(n) + 1; // Math.floor() cuts off the decimal point while +1 adds 1 to the value of n.
+console.log(n);
