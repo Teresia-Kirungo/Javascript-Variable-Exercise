fb5dcd21b3d19228bd5dc349dd3d4b52c44be9a8
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-01 08:44:40 +0300

    Update Love Calculator.js

--- Love Calculator.js
+++ Love Calculator.js
@@ -1,1 +1,8 @@
+name1 = prompt("What is your name?");
+name2 = prompt("What is the name of your crush?");
 
+n = Math.random();
+n = n * 100;
+n = Math.floor(n);
+
+console.log( n + "%" + " is the love percentage between " + name1 + " and " + name2 " ." );
